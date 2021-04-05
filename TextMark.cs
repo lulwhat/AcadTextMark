1c2c9fd2e431c8874e27dd739b85e53db9995ad4
evgen <frostzzz@bk.ru>
2021-04-05 09:41:56 +0500

    added top and bottom text markings. 2 cases of them are still in maintenance

--- TextMark.cs
+++ TextMark.cs
@@ -12,10 +12,10 @@ namespace AcadTextPlacement
 {
     public class TextMark
     {
-
+        // class that saves previosly used options for TextMark
         public class DataSaver
         {
-            public const string key = "scaleDef";
+            public const string scaleKey = "scaleDef";
 
             public int scale_saved;
             public DataSaver()
@@ -32,17 +32,17 @@ namespace AcadTextPlacement
             Database db = doc.Database;
             Editor ed = doc.Editor;
             Hashtable ud = doc.UserData;
-            DataSaver ds;
+            DataSaver dss;
 
-            // Initiate previously used scale saver
-            ds = ud[DataSaver.key] as DataSaver;
-            if (ds == null)
+            // Initiate data saver
+            dss = ud[DataSaver.scaleKey] as DataSaver;
+            if (dss == null)
             {
-                object obj = ud[DataSaver.key];
+                object obj = ud[DataSaver.scaleKey];
                 if (obj == null)
                 {
-                    ds = new DataSaver();
-                    ud.Add(DataSaver.key, ds);
+                    dss = new DataSaver();
+                    ud.Add(DataSaver.scaleKey, dss);
                 }
             }
 
@@ -63,11 +63,24 @@ namespace AcadTextPlacement
 
             PromptStringOptions txt = new PromptStringOptions("Введите текст");
             txt.Message = "\nВведите текст для маркировки линий: ";
-            txt.AllowSpaces = false;
+            txt.AllowSpaces = true;
             PromptResult txt_result = ed.GetString(txt);
             String txt_value = txt_result.StringResult;
 
             if (txt_result.Status != PromptStatus.OK)
+                return;
+            
+            // Ask user for text position
+
+            PromptKeywordOptions pos = new PromptKeywordOptions(
+                "\nВыберите положение текста относительно линии[Центр/Верх/Низ]:",
+                "Центр Верх Низ"
+                );
+            pos.Keywords.Default = "Центр";
+            PromptResult pos_result = ed.GetKeywords(pos);
+            String pos_value = pos_result.StringResult;
+
+            if ((pos_result.Status != PromptStatus.OK) & (pos_result.Status != PromptStatus.Keyword))
                 return;
 
 
@@ -82,10 +95,10 @@ namespace AcadTextPlacement
             scale.Keywords.Add("5000");
             scale.AppendKeywordsToMessage = true;
             scale.UseDefaultValue = true;
-            scale.DefaultValue = ds.scale_saved;
+            scale.DefaultValue = dss.scale_saved;
             PromptIntegerResult scale_result = ed.GetInteger(scale);
             int scale_value = scale_result.Value;
-            ds.scale_saved = scale_value;
+            dss.scale_saved = scale_value;
 
 
             if (scale_result.Status != PromptStatus.OK)
@@ -116,7 +129,7 @@ namespace AcadTextPlacement
 
                     Point3dCollection entPts = new Point3dCollection();
                     List<double> rot = new List<double>();
-                    CollectPoints(tr, ent, entPts, rot, scale_value);
+                    CollectPoints(tr, ent, entPts, rot, scale_value, pos_value);
 
                     // Add a physical DBPoint at each Point3d
 
@@ -125,9 +138,20 @@ namespace AcadTextPlacement
                         DBText dbt = new DBText();
                         dbt.SetDatabaseDefaults();
                         dbt.Position = entPts[i];
-                        dbt.Height = scale_value / 100;
+                        dbt.Height = 2 / 1000.0 * scale_value;
                         dbt.Rotation = rot[i];
-                        dbt.Justify = AttachmentPoint.MiddleCenter;
+                        if ((pos_value == "Центр") | (pos_value == "Ц"))
+                        {
+                            dbt.Justify = AttachmentPoint.MiddleCenter;
+                        }    
+                        else if ((pos_value == "Низ") | (pos_value == "Н"))
+                        {
+                            dbt.Justify = AttachmentPoint.TopLeft;
+                        }
+                        else
+                        {
+                            dbt.Justify = AttachmentPoint.BottomLeft;
+                        }
                         dbt.AlignmentPoint = entPts[i];
                         dbt.TextString = txt_value;
                         btr.AppendEntity(dbt);
@@ -137,11 +161,11 @@ namespace AcadTextPlacement
                 tr.Commit();
             }
         }
-        private void CollectPoints(Transaction tr, Entity ent, Point3dCollection pts, List<double> rot, int scale_value)
+        private void CollectPoints(Transaction tr, Entity ent, Point3dCollection pts, List<double> rot, int scale_value, String pos_value)
         {
             
             // Operations with polyline
-            // Iterate vertices and generate points for polyline segments with preset step (currently 100)
+            // Iterate vertices and generate points for polyline segments based on scale
 
             Polyline pline = ent as Polyline;
             if (pline != null)
@@ -156,7 +180,77 @@ namespace AcadTextPlacement
                         double dx = pt2.X - pt1.X;
                         double dy = pt2.Y - pt1.Y;
                         double d = Math.Sqrt(dx*dx + dy*dy);
+                        double offset_d = 0.0;
+                        double offset_x = 0.0;
+                        double offset_y = 0.0;
+                        // offset is added when text signs are either above or under the line
+                        // cases of line aligment that affect x and y offset
+                        if ((dy < 0) & (dx > 0))
+                        {
+                            if ((pos_value == "Верх") | (pos_value == "В"))
+                            {
+                                offset_d = 0.6 + (0.002 * scale_value);
+                                offset_x = Math.Cos(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                            }
+                            else if ((pos_value == "Низ") | (pos_value == "Н"))
+                            {
+                                offset_d = 0.6 + (0.002 * scale_value);
+                                offset_x = -(Math.Cos(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value);
+                                offset_y = -(Math.Sin(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value);
+                            }
+                        }
 
+                        //need adjustments
+                        else if ((dy < 0) & (dx < 0))
+                        {
+                            if ((pos_value == "Верх") | (pos_value == "В"))
+                            {
+                                offset_d = -(0.6 + (0.002 * scale_value));
+                                offset_x = Math.Cos(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.PI / 2 - Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                            }
+                            else if ((pos_value == "Низ") | (pos_value == "Н"))
+                            {
+                                offset_d = -(0.6 + (0.002 * scale_value));
+                                offset_x = Math.Cos(Math.PI / 2 + Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.PI / 2 + Math.Acos(dx / d)) * 0.5 / 1000.0 * scale_value;
+                            }
+                        }
+
+                        // need adjustments
+                        else if ((dx < 0) & (dy > 0))
+                        {
+                            if ((pos_value == "Верх") | (pos_value == "В"))
+                            {
+                                offset_d = -(0.6 + (0.002 * scale_value));
+                                offset_x = dx / d * 0.5 / 1000.0 * scale_value;
+                                offset_y = dy / d * 0.5 / 1000.0 * scale_value;
+                            }
+                            else if ((pos_value == "Низ") | (pos_value == "Н"))
+                            {
+                                offset_d = -(0.6 + (0.002 * scale_value));
+                                offset_x = Math.Cos(Math.Acos(dx / d) - Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.Acos(dx / d) - Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                            }
+                        }
+                        else
+                        {
+                            if ((pos_value == "Верх") | (pos_value == "В"))
+                            {
+                                offset_d = 0.6 + (0.002 * scale_value);
+                                offset_x = Math.Cos(Math.Acos(dx / d) + Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.Acos(dx / d) + Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                            }
+                            else if ((pos_value == "Низ") | (pos_value == "Н"))
+                            {
+                                offset_d = 0.6 + (0.002 * scale_value);
+                                offset_x = Math.Cos(Math.Acos(dx / d) - Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                                offset_y = Math.Sin(Math.Acos(dx / d) - Math.PI / 2) * 0.5 / 1000.0 * scale_value;
+                            }
+                        }
+
+                        // compute text rotation based on line coordinate sector position
                         double rotation = 0.0;
 
                         if ((dx > 0) && (dy > 0))
@@ -185,11 +279,19 @@ namespace AcadTextPlacement
                         }
 
                         if (d > scale_value / 40)
-                        
                         {
-                            for (double j = scale_value / 33; j < d - scale_value / 33; j += scale_value / 10)
+                            // j is a step on a line segment
+                            for (
+                                    double j = scale_value / 33;
+                                    j < d - scale_value / 33;
+                                    j += scale_value / 10
+                                )
                             {
-                                Point3d pt = new Point3d(pt1.X + dx * j / d, pt1.Y + dy * j / d, pt1.Z);
+                                Point3d pt = new Point3d(
+                                    pt1.X + (j + offset_d) * dx / d + offset_x,
+                                    pt1.Y + (j + offset_d) * dy / d + offset_y,
+                                    pt1.Z
+                                    );
                                 pts.Add(pt);
                                 rot.Add(rotation);
                             }
